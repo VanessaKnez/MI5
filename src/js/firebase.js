7961045e7135f739dc91c2e8f3da63549c640561
VanessaKnez <32335826+VanessaKnez@users.noreply.github.com>
2019-10-09 16:05:40 +0200

    Add files via upload

---
+++ src/js/firebase.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC1q3AYQ2uTukjSd98LYAbAr6oITWho3Dk",
+    authDomain: "mi5quiz.firebaseapp.com",
+    databaseURL: "https://mi5quiz.firebaseio.com",
+    projectId: "mi5quiz",
+    storageBucket: "",
+    messagingSenderId: "984292349621",
+    appId: "1:984292349621:web:0a2568229b712c72ef90fc",
+    measurementId: "G-TDNL2XK012"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
